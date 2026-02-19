aadb573bf2abf977bb211d704b1a66cee023b86b
NotSoHealthy <aminbenhamouda16@gmail.com>
2026-02-19 09:30:43 +0100

    Enable Eureka and configure gateway routes

--- backEnd/gateway/src/main/java/org/esprit/gateway/GatewayApplication.java
+++ backEnd/gateway/src/main/java/org/esprit/gateway/GatewayApplication.java
@@ -2,12 +2,33 @@ package org.esprit.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(GatewayApplication.class, args);
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("candidat",
+                        r->r.path("/candidat/**")
+                                .uri("lb://candidat"))
+                .route("job",
+                        r->r.path("/job/**")
+                                .uri("lb://Job"))
+                .route("candidature",
+                        r->r.path("/candidature/**")
+                                .uri("lb://candidature"))
+                .route("meeting",
+                        r->r.path("/meeting/**")
+                                .uri("lb://meeting"))
+                .route("notification",
+                        r->r.path("/notification/**")
+                                .uri("lb://notification"))
+                .build();
     }
-
 }
--- backEnd/microservices/meeting/src/main/java/org/esprit/meeting/MeetingApplication.java
+++ backEnd/microservices/meeting/src/main/java/org/esprit/meeting/MeetingApplication.java
@@ -2,8 +2,10 @@ package org.esprit.meeting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MeetingApplication {
 
     public static void main(String[] args) {
